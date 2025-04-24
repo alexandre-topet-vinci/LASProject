72b7c40ec28e263ea5e5aa65f341b07287228b12
alexandre-topet-vinci <alexandre.topet+1@gmail.com>
2025-04-24 08:51:01 +0200

    files push

---
+++ pas_server.c
@@ -0,0 +1,58 @@
+#include "utils_v3.h"
+
+#define KEY 84937
+#define PERM 0666
+
+void client_handler(int sig) {
+    // Handler pour le signal SIGUSR1  
+    int shm_id = sshmget(KEY, sizeof(int), IPC_CREAT | PERM);
+    int *distance = sshmat(shm_id);
+
+    // Vous pouvez ajouter du code ici si nécessaire    
+
+    sshmdt(distance); 
+    printf("Radar program finished.\n");
+}
+
+int main(int argc, char *argv[]) {
+    int shm_id = sshmget(KEY, sizeof(int), IPC_CREAT | PERM);
+    int *valeur = sshmat(shm_id);
+
+    int pipefdServerToBroadcast[2];
+    spipe(pipefdServerToBroadcast);
+    sclose(pipefdServerToBroadcast[0]);
+    struct GameState state;
+    FileDescriptor sout = 1;
+    FileDescriptor map  = sopen("./resources/map.txt", O_RDONLY, 0);
+    load_map(map, sout, &state);
+    sclose(map);
+
+    // implémentation de la logique du jeu ici
+
+    pid_t client1 = sfork();
+    if (client1 == 0) {
+        
+        client_handler();
+        exit(EXIT_SUCCESS);
+    }
+    
+    pid_t client2 = sfork();
+    if (client2 == 0) {
+        
+        client_handler();
+        exit(EXIT_SUCCESS);
+    }
+    pid_t broadcast = sfork();
+    if (broadcast == 0) {
+        // Code pour le processus de diffusion
+        
+        exit(EXIT_SUCCESS);
+    }
+
+    swaitpid(client1, NULL, 0);
+    swaitpid(client2, NULL, 0);
+
+    sshmdt(valeur);
+    sshmdelete(shm_id);
+    return 0;
+}
